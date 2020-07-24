77cd1051a9f29c5c94a5b7384f1a0bda56e12928
ApoorvBhardwaj15 <62135647+ApoorvBhardwaj15@users.noreply.github.com>
2020-07-25 02:49:14 +1000

    Update MiniCard.js

--- MiniCard.js
+++ MiniCard.js
@@ -3,13 +3,9 @@ import Card from 'react-bootstrap/Card';
 
 const MiniCard = (props) => {
 
-
 	return (
-
 		<div>
-
 			<Card>
-
 				<Card.Img variant="top" src={props.url1} alt={props.url2} onError={(e)=>{e.target.onerror = null; e.target.src=e.target.alt}}/>
 				<Card.Body>
 					<Card.Title variant="primary">${props.price} AUD</Card.Title>
